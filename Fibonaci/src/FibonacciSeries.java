01a523eba7e5fa8cac375e3b1d6ecc2d4d9b6325
vallivattikuti <valli27@gmail.com>
2018-05-24 00:43:35 -0400

    for tekarch
    for tekarch

---
+++ Fibonaci/src/FibonacciSeries.java
@@ -0,0 +1,14 @@
+
+public class FibonacciSeries {
+	
+	public static void main(String[] args) {
+		
+		int fib = 5;
+		for(int i=0;i<=5;i++)
+		{
+			int fs = i + i-1;
+			System.out.println(fs);
+		}
+	}
+
+}
